2754f7b56b3d5450287437f774b1acc65abf1efb
Kira <thiennguyen.fsd@gmail.com>
2023-12-17 15:50:21 +0700

    feat: implement gateway service

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -20,13 +20,26 @@ public static class Config
     public static IEnumerable<Client> Clients =>
         new Client[]
         {
-           new() {
+           new()
+           {
             ClientId = "postman",
             ClientName = "Postman",
             AllowedScopes = {"openid", "profile", "auctionApp"},
             RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
             ClientSecrets = new[] {new Secret("NotASecret".Sha256())},
             AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
+           },
+           new()
+           {
+            ClientId = "nextApp",
+            ClientName = "nextApp",
+            ClientSecrets = new[] {new Secret("secret".Sha256())},
+            AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+            RequirePkce = false,
+            RedirectUris = {"http://localhost:3000/api/auth/callback/id-server"},
+            AllowOfflineAccess = true,
+            AllowedScopes = {"openid", "profile", "auctionApp"},
+            AccessTokenLifetime = 3600*24*30
            }
         };
 }
---
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+  public async Task Consume(ConsumeContext<BidPlaced> context)
+  {
+    Console.WriteLine("--> Consuming bid placed " + context.MessageId);
+
+    var auction = await DB.Find<Item>().OneAsync(context.Message.Id);
+
+    if (context.Message.BidStatus.Contains("Accepted") &&
+      context.Message.Amount > auction.CurrentHighBid)
+    {
+      auction.CurrentHighBid = context.Message.Amount;
+      await auction.SaveAsync();
+    }
+  }
+}
